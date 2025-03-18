e043b06af4ef8c05d68b9f24894afc745a7b056c
dbsghrkd <yunho.kang@stonybrook.edu>
2025-03-18 07:20:10 -0400

    Add login & register page

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Main from "./pages/Main.jsx";
+import Sign_in from "./pages/Sign_in.jsx";
+import Sign_up from "./pages/Sign-up.jsx";
 
 function App() {
-
-  const scrollToSection = (id) => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' }); // 부드럽게 스크롤
-    }
-  }
-
   return (
     <div className="App">
-      <nav className="navBar">
-        <button href="App.js" className="logo">logo</button>
-        <ul>
-          <li><button href="#home" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Home</button></li>
-          <li><button href="#about" onClick={(e) => { e.preventDefault(); scrollToSection('about'); }}>About</button></li>
-          <li><button href="#">Sign In</button></li>
-          <li><button href="#">Trial for Guest</button></li>
-        </ul>
-      </nav>
       <header className="App-header">
-        {/* Background image content */}
-        <h1>Welcome to Our Service</h1>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/Sign-in" element={<Sign_in />} />
+            <Route path="/Sign-up" element={<Sign_up />} />
+          </Routes>
+        </Router>
       </header>
-
-      {/* About Section */}
-      <section id="about" className="aboutSection">
-        <div className="aboutWrapper">
-          <div className="aboutLeft">
-            <h2>About Life<br />Financial Planning</h2>
-          </div>
-          <div className="aboutRight">
-            <p>
-              Welcome to Life Financial Planning – your trusted partner in building a <strong>secure and prosperous</strong> future. 
-              We provide personalized financial strategies tailored to your unique goals, whether you're planning for retirement, 
-              managing investments, or securing your family's financial well-being. 
-              Our expert advisors combine in-depth knowledge with a client-first approach to help you navigate every stage of life with confidence.
-              Start your journey toward financial freedom today with Life Financial Planning – where your future is our priority.
-            </p>
-          </div>
-        </div>
-      </section>
     </div>
   );
 }
