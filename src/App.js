61e89fa694c735dec64a85cf7add4a37ea36f4cd
연어쏘는초밥 <hyeongseoksim@gmail.com>
2025-03-19 08:00:41 +0900

    basename test based on pipeline build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Main from "./pages/Main.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 
 function App() {
+  const buildType = process.env.REACT_APP_BUILD_TYPE;
+  const branch = process.env.REACT_APP_BRANCH;
+  // test 빌드인 경우 브랜치 이름을 basename으로 사용, 그렇지 않으면 기본 '/'
+  const basename = buildType === 'test' && branch ? `/${branch}` : "/";
+
   return (
     <div className="App">
       <header className="App-header">
-      <Router>
+      <Router basename={basename}>
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/SignIn" element={<SignIn />} />
