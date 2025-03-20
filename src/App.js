b1f43ff62607e19b5b477c8fa31d6286059f1cdd
연어쏘는초밥 <hyeongseoksim@gmail.com>
2025-03-20 11:13:07 +0900

    Test config

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import basename from "./config.js"
 import Main from "./pages/Main.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 
 function App() {
-  const buildType = process.env.REACT_APP_BUILD_TYPE;
-  const branch = process.env.REACT_APP_BRANCH;
-  // test 빌드인 경우 브랜치 이름을 basename으로 사용, 그렇지 않으면 기본 '/'
-  const basename = buildType === 'test' && branch ? `/${branch}` : "/";
+  console.log(basename);
 
   return (
     <div className="App">
       <header className="App-header">
-      <Router basename={basename}>
+      <Router>
           <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="SignIn" element={<SignIn />} />
-            <Route path="SignUp" element={<SignUp />} />
+            <Route path={`${basename}`} element={<Main />} />
+            <Route path={`${basename}/SignIn`} element={<SignIn />} />
+            <Route path={`${basename}/SignUp`} element={<SignUp />} />
           </Routes>
         </Router>
       </header>
     </div>
   );
 }
+
 
 export default App;
