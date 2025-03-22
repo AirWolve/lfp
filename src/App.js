2dc2a67c946f9ea0b09ff49f6820eb8d6c7b55a3
연어쏘는초밥 <hyeongseoksim@gmail.com>
2025-03-22 09:02:53 +0900

    Test Hash Router with config.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import basename from "./config.js"
 import Main from "./pages/Main.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 
 function App() {
-  console.log(basename);
-
   return (
     <div className="App">
       <header className="App-header">
