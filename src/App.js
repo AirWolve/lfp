dce34e2fcfa0cff9f1e65ea58431664f10c4033c
dbsghrkd <yunho.kang@stonybrook.edu>
2025-03-22 23:30:19 -0400

    Create Basic Setting Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import BasicSetting from "./pages/BasicSetting.jsx";
+import IncomeExpense from "./pages/IncomeExpense.jsx"
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/BasicSetting" element={<BasicSetting />} />
+          <Route path="/IncomeExpense" element={<IncomeExpense />} />
         </Routes>
       </header>
     </div>
