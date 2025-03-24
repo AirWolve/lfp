c084db0340177880e765a48d10e9b7110628c72b
연어쏘는초밥 <hyeongseoksim@gmail.com>
2025-03-24 15:57:50 +0900

    Fix error occurred by import

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { constPath } from '../config.js';
 import "./Profile.css";
