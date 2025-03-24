4de3dada2b4890f73a997a6fa655e947cea75367
junhypark <phjpurpleoob@gmail.com>
2025-03-23 21:16:10 -0400

    # Class create Scenarios, and some calculator part

--- python/Calculator.py
+++ python/Calculator.py
@@ -1,51 +1,199 @@
-class Calculator:
+import json
+import pandas as pd
+import datetime
+import numpy as np
+import requests
+
+class RMDCalculator:
+    """
+    This is for RMD Calculator
+    
+    Args:
+    ----
+        LFTTable = static path to read the json file which holds RMD ratio
+        yaer = today's year integer value
+    """
     def __init__(self):
-        LFTTable = ""   # scrape or something for the LFT table
-
+        with open('./LFTTable.json') as f:
+            self.LFTTable = json.loads(f.read()).copy()   # scrape or something for the LFT table
+        self.year = datetime.date.today().year
     # RMD Calculator
-    def RMDCalculator(self, accountBalance, age):
-        if age > 120:
-            return accountBalance / self.LFTTable.get(120)
+    def runRMD(self, data:dict):
+        df_invs = pd.DataFrame(data["investments"]).copy()
+        age = data["birthYears"][0]-self.year
+        
+        if age < 74:
+            return 0
         else:
-            return accountBalance / self.LFTTable.get(age)
+            for idn in data["RMDStrategy"]:
+                if age > 120:
+                    return df_invs[df_invs["id"]==idn]["value"].item() / self.LFTTable.get(str(120))
+                else:
+                    return df_invs[df_invs["id"]==idn]["value"].item() / self.LFTTable.get(str(age))
 
-    # Inflation Assumption
-    def inflationAssumption(initialYear, expenseYearAmount, initialAmount, inflationRate = 3.0):
-        if initialYear == True:
-            return initialAmount * (1+inflationRate)
-        return expenseYearAmount * (1+inflationRate)
+class TaxCalculator:
+    def __init__(self):
+        self.apiKey = ""
     
     # Tax Calculator
-    def taxCalculator():
-        pass
+    def taxCalculator(self, data:dict, amount:int, country:str|None = 'US'):
+        region = data["residenceState"]
+        filing_status = 'married' if data["maritalStatus"] == "couple" else 'single'
+        api_url = 'https://api.api-ninjas.com/v1/incometaxcalculator?country={}&region={}&income={}&filing_status={}'.format(country, region, amount, filing_status)
+        res = requests.get(api_url, headers={'X-Api-Key': self.apiKey["key"]})
 
-    # Expense Withdrawal Strategy
-    def expenseWithdrawal(initialAmount, annualReturn, yearRest):
-        return initialAmount*(annualReturn*(1+annualReturn)^yearRest)/((1+annualReturn)^yearRest-1)
+        if res.status_code == requests.codes.ok:
+            return res.text
+        else:
+            raise RuntimeError(f"Error: {res.status_code}, {res.text}")
     
-    # roth conversion
-    def rothConversion (startAmount, annualContribution, age, retirementAge, investmentReturn):
-        '''
-        function returns two variables
-        retirementBalance, totalContribution
-        Shoulde unzip or zip the return variables
-        '''
-        total = startAmount
-        totalContribution = 0.0
-        retirementBalance = 0.0
+    def scrapeTable(self, data:dict, country:str|None = 'US'):
+        filePath = './tex_income.json'
 
-        for i in range(age, retirementAge):
-            total = (total + annualContribution) * (1+investmentReturn / 100)
-            totalContribution += annualContribution
-            retirementBalance = total
-        return retirementBalance, totalContribution
+        year = datetime.date.today().year
+        api_url = 'https://api.api-ninjas.com/v1/incometax?country={}&year={}&federal_only={}'.format(country, year, data["residenceState"])
+        
+        res = requests.get(api_url, headers={'X-Api-Key': self.apiKey})
+        
+        if res.status_code == requests.codes.ok:
+            with open(filePath, 'w', encoding="utf-8") as f:
+                json.dump(res.text, f, ensure_ascii=False, indent="\t")
+        
+        return filePath
 
-    # Investment Calculator is considered to be modified
-
-    # class Investment:
-    #     id = 0
-    #     def __init__(self):
-    #         name = ""
-    #         currentValue = 0.0
-    #         riskFactor = 0.0
+# Inflation Assumption
+class Inflation:
+    def __init__(self, data:dict):
+        self.type = data["inflationAssumption"]["type"].copy()
+        self.assumption = data["inflationAssumption"].copy()
+        self.data = data.copy()
     
+    def baseInflation(self):
+        """
+        This function is for base inflation assumption
+
+        returns list of dictionary of series inflation assumed amount values
+        """
+        result = list()
+        df_es = pd.DataFrame(self.data["eventSeries"]).copy()
+        curYear = datetime.date.today().year
+
+        for idx, row in df_es.iterrows():
+            values = list()
+            if row["inflationAdjusted"].item() == "false":
+                continue
+            else:
+                duration = row["duration"]["value"].item()
+                
+                if row["start"]["type"].item() == "fixed":
+                    start = row["start"]["value"].item()
+                    
+                    if start == curYear:
+                        values.append(row["initialAmount"].item() * (1-self.assumption["value"].item()))
+                    else:
+                        if not values:
+                            for i in range(start, curYear):
+                                values.append(None)
+
+                        for i in range(duration):
+                            values.append(values[-1]*(1-self.assumption["value"].item())**len(values))
+                        
+                elif row["start"]["type"].item() == "startWith":
+                    start = df_es[df_es["name"] == row["start"]["eventSeries"]]["start"]["value"].item()
+
+                    if start == curYear:
+                        values.append(row["initialAmount"].item() * (1-self.assumption["value"].item()))
+                    else:
+                        if not values:
+                            for i in range(start, curYear):
+                                values.append(None)
+
+                        for i in range(duration):
+                            values.append(values[-1]*(1-self.assumption["value"].item())**len(values))
+                
+                elif row["start"]["type"].item() == "startAfter":
+                    start = df_es[df_es["name"] == row["start"]["eventSeries"]]["start"]["value"].item()
+
+                    if start == curYear:
+                        values.append(row["initialAmount"].item() * (1-self.assumption["value"].item()))
+                    else:
+                        if not values:
+                            for i in range(start, curYear):
+                                values.append(None)
+
+                        for i in range(duration):
+                            values.append(values[-1]*(1-self.assumption["value"].item())**len(values))
+            
+            result.append({row["name"]: values})
+
+        return result
+
+
+    def uniformInflation(self):
+        return None
+
+    def normalInflation(self):
+        return None
+
+    def runInflation(self):
+        if self.type == "fixed":
+            self.baseInflation()
+        elif self.type == "uniform":
+            self.uniformInflation()
+        elif self.type == "normal":
+            self.normalInflation()
+        else:
+            raise ValueError("Invalid Type of Inflation Assumption!")
+"""
+Re-start
+
+def inflationAssumption(initialYear, expenseYearAmount, initialAmount, inflationRate = 3.0):
+    if initialYear == True:
+        return initialAmount * (1+inflationRate)
+    return expenseYearAmount * (1+inflationRate)
+"""
+
+# Expense Withdrawal Strategy
+def expenseWithdrawal(initialAmount, annualReturn, yearRest):
+    return initialAmount*(annualReturn*(1+annualReturn)^yearRest)/((1+annualReturn)^yearRest-1)
+
+# roth conversion
+def rothConversion (startAmount, annualContribution, age, retirementAge, investmentReturn):
+    '''
+    function returns two variables
+    retirementBalance, totalContribution
+    Shoulde unzip or zip the return variables
+    '''
+    total = startAmount
+    totalContribution = 0.0
+    retirementBalance = 0.0
+
+    for i in range(age, retirementAge):
+        total = (total + annualContribution) * (1+investmentReturn / 100)
+        totalContribution += annualContribution
+        retirementBalance = total
+        
+    return list(retirementBalance, totalContribution)
+
+def runCalculator(file:str):
+    """
+    This is a function to run all calculator as possible as can
+
+    Args:
+    ----
+        file: This is a path for the pkl file to load and use
+    """
+
+    with open(file) as f:
+        data = json.loads(f.read())
+    return RMDCalculator.runRMD(data)
+
+
+# Investment Calculator is considered to be modified
+
+# class Investment:
+#     id = 0
+#     def __init__(self):
+#         name = ""
+#         currentValue = 0.0
+#         riskFactor = 0.0
--- python/Scenario.py
+++ python/Scenario.py
@@ -1,8 +1,45 @@
+import yaml
+import pickle
+import os
+from Calculator import RMDCalculator
+import Calculator
+
 class Scenario:
-    id = 0
+    """
+    a distribution is represented as a map with one of the following forms:
+    {type: fixed, value: <number>}
+    {type: normal, mean: <number>, stdev: <number>}
+    {type: uniform, lower: <number>, upper: <number>}
+    {type: GBM, mu: <number>, sigma: <number>}
+    percentages are represented by their decimal value, e.g., 4% is represented as 0.04.
+    """
     def __init__(self, parameters):
-        self.parameters = parameters
-    
+        """
+        This is a Scenario class init function
+
+        Args:
+        ----
+            parameters: the dictionary of all yaml or json data
+        """
+        self.name:str = parameters["name"]
+        self.maritalStatus:str = parameters["maritalStatus"]
+        self.birthYears:list = parameters["birthYears"].copy()
+        self.lifeExpectancy:list = parameters["lifeExpectancy"].copy()
+        self.investmentTypes:list = [InvestmentTypes(params) for params in parameters["investmentTypes"]]
+        self.investments:list = [Investments(params) for params in parameters["investments"]]
+        self.eventSeries:list = [EventSeries(params) for params in parameters["eventSeries"]]
+        self.inflationAssumption:dict = parameters["inflationAssumption"].copy()
+        self.afterTaxContributionLimit:int = parameters["afterTaxContributionLimit"]
+        self.spendingStrategy:list = parameters["spendingStrategy"].copy()
+        self.expenseWithdrawalStrategy:list = parameters["expenseWithdrawalStrategy"].copy()
+        self.RMDStrategy:list = parameters["RMDStrategy"].copy()
+        self.RothConversionOpt:bool = parameters["RothConversionOpt"]
+        self.RothConversionStart:int = parameters["RothConversionStart"]
+        self.RothConversionEnd:int = parameters["RothConversionEnd"]
+        self.RothConversionStrategy:list = parameters["RothConversionStrategy"].copy()
+        self.financialGoal:int = parameters["financialGoal"]
+        self.residenceState:str = parameters["residenceState"]
+
     def getParameters(self, key):
         return self.parameters.get(key)
     
@@ -12,3 +49,161 @@ class Scenario:
             self.parameters[key] = value
         else:
             print("Invalid Setting!")
+    
+class InvestmentTypes:
+    def __init__(self, parameters:dict):
+        self.name:str = parameters["name"]
+        self.description:str = parameters["description"]
+        self.returnAmtOrPct:str = parameters["returnAmtOrPct"]    # amount or percent
+        self.returnDistribution:dict = parameters["returnDistribution"].copy()
+        self.expenseRatio:int = parameters["expenseRatio"]
+        self.incomeAmtOrPct:str = parameters["incomeAmtOrPct"]
+        self.incomeDistribution:dict = parameters["incomeDistribution"].copy()
+        self.taxability:bool = parameters["taxability"]
+
+class Investments:
+    def __init__(self, parameters:dict):
+        self.investmentType:str = parameters["investmentType"]
+        self.value:int = parameters["value"]
+        self.taxStatus:str = parameters["taxStatus"]  # "non-retirement", "pre-tax", or "after-tax"
+        self.id:str = parameters["id"]
+
+class EventSeries:
+    """
+    This is a class for the EventSeries in Yaml file
+
+    Args:
+    ----
+        name: this is a name for event
+        start: this is a list containing type of distribution
+            type: fixed, normal, uniform, startWith, startAfter
+            ex) {type: fixed, vlaue: 2025}
+                {type: startWith, eventSeries: salary}
+        duration: this is a dictionary containing type and duration value
+            if value exceeded the lifetime, it lasts for the rest of the user's life
+            ex) {type: fixed, value: 200}
+                {type: fixed, value: 40}
+        type: this is type of event, there are four types (income, expense, invest, or rebalance)
+        initialAmount: this is a initialAmount of the event
+        changeAmtOrPct: this is a change amount or percentage
+        changeDistribution: this is a distribution setting in dict form
+            ex) {type: uniform, lower: 500, upper: 2000}
+                {type: normal, mean: 0.02, stdev: 0.01}
+                {type: fixed, value: 0}
+        inflationAdjusted: this is a boolean value whether adjust inflation or not
+        userFraction: this is a fraction of the amount associated with the user.
+        socialSecurity: this is a boolean
+    """
+    def __init__(self, parameters:dict):
+        if parameters["type"] == "invest":
+            Invest(parameters)
+        elif parameters["type"] == "rebalance":
+            Rebalance(parameters)
+        else:    
+            self.name:str = parameters["name"]
+            self.start:dict = parameters["start"].copy()
+            self.duration:dict = parameters["duration"].copy()
+            self.type:str = parameters["type"]    # income, expense, invest, or rebalance
+            self.initialAmount:int = parameters["initialAmount"]
+            self.changeAmtOrPct:str = parameters["changeAmtOrPct"]
+            self.changeDistribution:dict = parameters["changeDistribution"].copy()
+            self.inflationAdjusted:bool = parameters["inflationAdjusted"]
+            self.userFraction:float = parameters["userFraction"]
+            if parameters["type"] == "income":
+                self.socialSecurity:bool = parameters["socialSecurity"]
+            else:
+                self.discretionary:bool = parameters["discretionary"]
+
+class Invest:
+    """
+    This is a class when there is a invest in the event series
+
+    Args:
+    ----
+        name: this is a name of invest
+        start: this is a dictionary of start value
+            ex) {type: uniform, lower: 2025, upper: 2030}
+        duration: this is a dictionary of duration value
+            ex) {type: fixed, value: 10}
+        type: this is a string to check whether it is a invest or not
+        assetAllocation: this is a dictionary of asset
+            ex) {S&P 500 non-retirement: 0.6, S&P 500 after-tax: 0.4}
+        glidePath: this is a boolean. False means assetAllocation is the fixed asset allocation and assetAllocation2 is unused
+                    True means using assetAllocation2 is used
+        maxCash: this is a integer max cash
+    """
+
+    def __init__(self, parameters:dict):
+        if parameters["type"] != "invest":
+            raise ValueError("Invalid input for the Invest Class!")
+        
+        self.name:str = parameters["name"]
+        self.start:dict = parameters["start"].copy()
+        self.duration:dict = parameters["duration"].copy()
+        self.type:str = parameters["type"]
+        self.assetAllocation:dict = parameters["assetAllocation"].copy()
+        self.glidePath:bool = parameters["glidePath"]
+        self.maxCash:int = parameters["maxCash"]
+
+        if self.glidePath is True:
+            self.assetAllocation2:dict = parameters["assetAllocation2"]
+
+class Rebalance:
+    """
+    This is a class for the event type rebalance
+
+    Args:
+    ----
+        name: this is a string for event name
+        start: this is a dictionary of start value
+            ex) {type: uniform, lower: 2025, upper: 2030}
+        duration: this is a dictionary of duration value
+            ex) {type: fixed, value: 10}
+        type: this is a type to check whether it is rebalance or not
+        assetAllocation: this is a dictionary of asset
+            ex) {S&P500 non-retirement: 0.7, tax-exempt bonds: 0.3}
+    """
+    def __init__(self, parameters:dict):
+        if parameters["type"] != "rebalance":
+            raise ValueError("Invalid input for the Rebalance Class!")
+        
+        self.name:str = parameters["name"]
+        self.start:dict = parameters["start"].copy()
+        self.duration:dict = parameters["duration"].copy()
+        self.type:str = parameters["type"]
+        self.assetAllocation:dict = parameters["assetAllocation"].copy()
+
+def importJson(file:str):
+        """
+        This is a function for importing JSON file
+
+        Args:
+        ----
+            file: this is full path for the JSON file
+
+        Structure of JSON file is following:
+        ----
+            name: String of scenario name
+            maritalStatus: couple or individual
+            birthYears: a list with lenght 1 or 2, depending on maritalStatus
+            lifeExpectancy: a list with length 1 or 2, depending on maritalStatus
+            investmentTypes: [list]
+            investments: [list]
+            eventSeries: [list]
+            inflationAssumption: {type, value}
+            afterTaxContributionLimit
+            spendingStrategy: list of discretionary expenses, identified by name
+            expenseWithdrawalStrategy: list of investments, identified by id
+            RMDStrategy: list of pre-tax investments, identified by id
+            RothConversionOpt: boolean indicating whether the roth conversion optimizer is enabled
+            RothConversionStart: start year
+            RothConversionEnd: end year
+            RothConversionStrategy: list of pre-tax investments, identified by id
+            financialGoal: goal amount
+            residenceState: states are identified by standard 2-letter abbreviations
+        """
+
+        with open(file) as f:
+            yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+        
+        return Scenario(yaml_data)
