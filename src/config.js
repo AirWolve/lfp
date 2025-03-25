166e1ee54d6adc6d8aadee484dbad3beee2a6e46
dbsghrkd <yunho.kang@stonybrook.edu>
2025-03-24 20:32:43 -0400

    Create Investments page

--- src/config.js
+++ src/config.js
@@ -11,6 +11,7 @@ export const constPath = {
     profile: `${baseDir}Profile`,
     dashboard: `${baseDir}Dashboard`,
     basicSetting: `${baseDir}BasicSetting`,
+    investments: `${baseDir}Investments`,
     overview: `${baseDir}Overview`,
     incomeExpense: `${baseDir}IncomeExpense`,
     cashFlow: `${baseDir}CashFlow`,
